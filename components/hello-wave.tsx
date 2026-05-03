122daac33b3bcb58acf82d00a78ddde57644d631
whosgonnatellarya <your-verified-email>
2026-05-03 12:59:51 +0400

    initial commit

--- components/hello-wave.tsx
+++ components/hello-wave.tsx
@@ -5,7 +5,7 @@ export function HelloWave() {
     <Animated.Text
       style={{
         fontSize: 28,
-        lineHeight: 32,
+        lineHeight: 36,
         marginTop: -6,
         animationName: {
           '50%': { transform: [{ rotate: '25deg' }] },
